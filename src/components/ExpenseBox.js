5b3ec66405ad7eef4cdf4062e076d096ba2ffd6e
DBardin <dbardin@croc.ru>
2021-10-09 18:40:19 +0300

    change to map

--- src/components/ExpenseBox.js
+++ src/components/ExpenseBox.js
@@ -3,21 +3,9 @@ import ExpenseItem from "./ExpenceItem";
 function ExpenseBox(props) {
   return (
     <div>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        date={props.expenses[0].date}
-        amount={props.expenses[0].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[1].title}
-        date={props.expenses[1].date}
-        amount={props.expenses[1].amount}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.expenses[2].title}
-        date={props.expenses[2].date}
-        amount={props.expenses[2].amount}
-      ></ExpenseItem>
+      {props.expenses.map((p) => {
+        return <ExpenseItem {...p} />;
+      })}
     </div>
   );
 }
